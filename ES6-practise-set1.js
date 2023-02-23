00fac9c8ae18fc2db027d7b4e87b691a39886a4b
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-02-23 21:11:15 +0530

    updated solution

--- ES6-practise-set1.js
+++ ES6-practise-set1.js
@@ -16,15 +16,34 @@ console.log(isEligibleToVote(17)) // Not eligible to vote
 //short-hand/ arrow function
 const isEligibleToVote = age => age>=18 ? "Eligible to vote" : " Not Eligible to vote";
 
-//2. Write a function that takes two numbers as input and determines which one is greater.
 
+//2. Write a function that takes two numbers as input and determines which one is greater.
 const isGreater = (num1,num2) => num1>num2 ? `${num1} is greater than ${num2}` : `${num2} is greater than ${num1}`
+
+const isGreater = (num1,num2) =>{
+    if(num1>num2){
+       return `${num1} is greater than ${num2}` 
+    } else if(num1<num2){
+        return `${num2} is greater than ${num1}`
+    }else{
+        return `both are equal`
+    }
+}
 console.log(isGreater(2, 5)) // 5 is greater than 2
 console.log(isGreater(10, 5)) // 10 is greater than 5 
 
 //3. Write a function that takes a number as input and determines if it is positive or negative.
 const checkNum = num => num>0 ? "positive number" : "negative number"
 
+const checkNum = num =>{
+    if(num>0){
+        return `positive number`
+    }else if(num<0){
+        return `negative number`
+    } else{
+        return `The number is 0`
+    }
+}
 console.log(checkNum(9)) // Positive Number
 console.log(checkNum(-8)) // Negative Number
 console.log(checkNum(22)) // Positive Number
@@ -33,13 +52,20 @@ console.log(checkNum(22)) // Positive Number
 
 const isEvenOdd = num => num%2 === 0 ? "even number" : "odd number";
 
+const isEvenOdd = num =>{
+    if (num%2 === 0){
+        return `even number`
+    }else{
+        return `oddd number`
+    }
+}
 console.log(isEvenOdd(5)) // Odd Number
 console.log(isEvenOdd(8)) // Even Number
 console.log(isEvenOdd(10)) // Even Number
 
 //5. Write a function that takes a string as input and determines if it contains the letter 'a' or ‘A’.
 
-const checkForAlphabetA = str => str.split("").includes("a"||"A") ? "includes A": "doesnot include A"
+// const checkForAlphabetA = str => str.split("").includes("a"||"A") ? "includes A": "doesnot include A"
 const checkForAlphabetA = str =>{
     let flag=0
     for(let i=0;i<str.length;i++){
@@ -58,9 +84,7 @@ const checkForAlphabetA = str =>{
 }
 console.log(checkForAlphabetA("Tanay")) // Includes a 
 console.log(checkForAlphabetA("Jeep")) // Does not include a 
-console.log(checkForAlphabetA("Jane")) // Includes a sole.log(checkForAlphabetA("Tanay")) // Includes a 
-console.log(checkForAlphabetA("Jeep")) // Does not include a 
-console.log(checkForAlphabetA("Jane")) // Includes a 
+console.log(checkForAlphabetA("Jane")) // Includes a  
 
 //6. Write a function that takes a string as input and determines if it is longer than 5 characters.
 
@@ -73,6 +97,16 @@ console.log(checkLength("Jeep")) // less than 5 characters
 //7. Write a function that takes a number as input and determines if it is between 1 and 10.
 const isBetweenOneAndTen = num => num>0 && num<=10 ? true : false;
 
+const isBetweenOneAndTen = num =>{
+    if(num>0 && num>=10){
+        return true;
+    }else 
+    
+    
+    {
+        return false;
+    }
+}
 console.log(isBetweenOneAndTen(5)); // true
 console.log(isBetweenOneAndTen(11)); // false
 
@@ -80,6 +114,14 @@ console.log(isBetweenOneAndTen(11)); // false
 //8. Write a function that takes a string as input and determines if it contains the word "hello".
 const isHelloPresent = str => str.toLowerCase().indexOf("hello") === -1 ? false: true;
 
+const isHelloPresent = str =>{
+    const lowerStr = str[i].toLowerCase()
+    for(let i=0;i<str.length;i++){
+        if( lowerStr[i] === "h"){
+            if()
+        }
+    }
+}
 console.log(isHelloPresent("Hello World")) // true
 console.log(isHelloPresent("World")) // false
 
