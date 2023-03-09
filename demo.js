ade3ac2a140167149cdda7cf7ba3e079cbba3086
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-03-09 19:26:47 +0530

    feat: added new prob

--- demo.js
+++ demo.js
@@ -110,4 +110,28 @@
 
 //--------------------
 
+//09/03
+```ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+```
+const validatePIN = input => {
+  if ( input.length === 4 || input.length === 6){
+    for(let i of input){
+      if (i<9 && i>=0){
+        return true
+      }else{
+        return false
+      }
+    }
+  }else{
+    return false
+  }
+}
+
+console.log(validatePIN('1.3234'))
