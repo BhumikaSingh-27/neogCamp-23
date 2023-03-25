9e53d2661357ca3abcfa01cf95516264fc9a037f
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-03-25 23:13:04 +0530

    Async await set 2

--- Promises-Async-Await/Practice-set 2/ques1.js
+++ Promises-Async-Await/Practice-set 2/ques1.js
@@ -1,31 +1,76 @@
+//Use this URL - https://example.com/post/comments to make a fake fetch call and
+//list out all the emails of users on the DOM in ordered list.
+//A fakeFetch has been provided. Use HTML, CSS & JS template in REPL or Vanilla template in CodeSandbox for this question.
+
+const showOutput = document.querySelector("#output");
+
 const fakeFetch = (url) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (url === "https://example.com/post/comments") {
-          resolve({
-            status: 200,
-            message: "Success",
-            data: [
-              { email: "xyz@gmail.com", commentBody: "Well done keep it up" },
-              {
-                email: "hello@outlook.com",
-                commentBody: "Good going a lot to learn from you awesome"
-              },
-              {
-                email: "ram99@gmail.com",
-                commentBody: "Anyone from Vadodara here"
-              }
-            ]
-          });
-        } else {
-          reject({
-            status: 404,
-            message: "No Commentes found."
-          });
-        }
-      }, 2000);
-    });
-  };
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (url === "https://example.com/post/comments") {
+        resolve({
+          status: 200,
+          message: "Success",
+          data: [
+            { email: "xyz@gmail.com", commentBody: "Well done keep it up" },
+            {
+              email: "hello@outlook.com",
+              commentBody: "Good going a lot to learn from you awesome",
+            },
+            {
+              email: "ram99@gmail.com",
+              commentBody: "Anyone from Vadodara here",
+            },
+          ],
+        });
+      } else {
+        reject({
+          status: 404,
+          message: "No Commentes found.",
+        });
+      }
+    }, 2000);
+  });
+};
 
+// fakeFetch("https://example.com/post/comments")
+//   .then((response) => {
+//     // console.log(response);
+//     let tag = "";
+//     for (let data of response.data) {
+//       tag += `<li>${data.email}</li>`;
+//     //   console.log(tag);
+//     }
+//     showOutput.innerHTML = `<ol>${tag}</ol>`;
+//   })
+//   .catch((error) => (showOutput.innerText = "error occured!"));
 
-  fakeFetch("https://example.com/post/comments").then((data)=> data.data.map(({email})=>console.log(email)))
+// const callback = async () => {
+//   try {
+//     let response = await fakeFetch("https://example.com/post/comments");
+//     let tag = "";
+//     for (let data of response.data) {
+//       tag += `<li>${data.email}</li>`;
+//     }
+//     showOutput.innerHTML = `<ol>${tag}</ol>`;
+//   } catch (error) {
+//     showOutput.innerText = "error!";
+//   }
+// };
+
+// callback();
+
+const callback = async () => {
+  try {
+    let response = await fakeFetch("https://example.com/post/comments");
+    let tag = response.data.reduce(
+      (acc, cur) => acc + `<li>${cur.email}</li>`,
+      ""
+    );
+    showOutput.innerHTML = `<ol>${tag}</ol>`;
+  } catch (error) {
+    showOutput.innerText = "error!";
+  }
+};
+
+callback();
---
+++ Promises-Async-Await/Practice-set 2/ques2.js
@@ -0,0 +1,75 @@
+// Use this URL - https://example.com/winner-team to make a fake fetch call to the
+// get names of winner group members and show a nice message on the DOM congratulating all of them. Use HTML, CSS & JS template in REPL or Vanilla template in CodeSandbox for this question.
+
+const showOutput = document.querySelector("#output");
+
+const fakeFetch = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (url === "https://example.com/winner-team") {
+        resolve({
+          status: 200,
+          data: {
+            message: "Success",
+            data: ["Jhon", "Raju", "Anjali", "Sakshi"],
+          },
+        });
+      } else {
+        reject({
+          status: 404,
+          message: "No Users found.",
+        });
+      }
+    }, 2000);
+  });
+};
+
+// const asyncFunction = async () => {
+//     try{
+//   let response = await fakeFetch("https://example.com/winner-team");
+//   const names = response.data.data;
+//   //   const winner = [...names];
+//   //   console.log(names);
+//   //   console.log(winner);
+//   showOutput.innerText = `Congratulation to the members of winning team ${names}, great work keep it up`;
+// }catch(error){
+//     showOutput.innerText="error ocuured";
+// }
+// }
+// asyncFunction();
+
+// const asyncFunction = async () => {
+//   try {
+//     let response = await fakeFetch("https://example.com/winner-team");
+//     const tag = response.data.data.reduce(
+//       (acc, cur, index, arr) =>
+//         index !== arr.length - 1 ? acc + cur + "," : acc + "and" + cur,
+//       ""
+//     );
+//     showOutput.innerText = `Congratulation to the members of winning team ${tag}great work keep it up`;
+//   } catch (error) {
+//     showOutput.innerText = "error occured!";
+//   }
+// };
+// asyncFunction();
+// Output on the DOM should be:
+// Congratulation to the members of winning team Jhon, Raju, Anjali, Sakshi, great work keep it up.
+
+const asyncFunction = async () => {
+  try {
+    let response = await fakeFetch("https://example.com/winner-team");
+    const names = response.data.data;
+    let tag = "";
+    for (let i = 0; i < names.length; i++) {
+      if (i === names.length - 1) {
+        tag = tag + "and " + names[i];
+      } else {
+        tag = tag + names[i] + ", ";
+      }
+    }
+    showOutput.innerText = `Congratulation to the members of winning team ${tag} great work keep it up`;
+  } catch (error) {
+    showOutput.innerText = "error occured!";
+  }
+};
+asyncFunction();
