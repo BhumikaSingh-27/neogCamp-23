96c37b5c63de89804cef6874d4b4dd935aeef47e
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-04-11 11:54:27 +0530

    feat: component added

--- react-set-7/src/components/Ques1.jsx
+++ react-set-7/src/components/Ques1.jsx
@@ -1,47 +1,69 @@
-import { useState } from "react"
-import { fakeFetch } from "../api/api1"
-import { useEffect } from "react"
+import { useState } from "react";
+import { fakeFetch } from "../api/api1";
+import { useEffect } from "react";
 
 export const Ques1 = () => {
+  const [weatherData, setWeatherData] = useState([]);
+  const [tempVar, setTempVar] = useState({ data: null, isFaren: false });
+  const [degree, setDegree] = useState("°C");
 
-const [ weatherData, setWeatherData ] = useState([])
-const [ tempVar, setTempVar ] = useState(0)
-const getData = async(url) =>{
+  const getData = async (url) => {
     try {
-        const response = await fakeFetch(url)
-        
-        if(response.status === 200){
-            setWeatherData(response.data)
-            setTempVar(response.data.temperature)
-        }
-        }catch(e){
-            console.log(e)
+      const response = await fakeFetch(url);
 
+      if (response.status === 200) {
+        setWeatherData([response.data]);
+        setTempVar({ ...tempVar, data: response.data.temperature });
+      }
+    } catch (e) {
+      console.log(e);
     }
-    console.log(tempVar)
-}
+  };
 
-const toggleValue = (e) => {
-    e.target.innerText="Switch to Celcius"
-    const cal = tempVar*1.8 + 32
-    setTempVar(cal)
-}
-useEffect(()=>{
-    getData("https://example.com/api/weather")
-},[])
-    return (
-        <div>
-            <h1>Weather</h1>
-            
-           {
-            <>
-            <p>temperature:  {tempVar}</p>
-            <p>Humidity: {weatherData.humidity} %</p>
-            <p>Wind Speed: {weatherData.windSpeed }Km/h</p>
-            </>
-           }
-           <button onClick={(e)=>(toggleValue)}>Switch to Fahrenheit</button> 
-           
-        </div>
-    )
-}
+  const toggleValue = (e) => {
+    if (!tempVar.isFaren) {
+      e.target.innerText = "Switch to Celcius";
+      setDegree("°F");
+    } else {
+      e.target.innerText = "Switch to Fahrenheit";
+      setDegree("°C");
+    }
+    setTempVar((prev) => {
+      if (!tempVar.isFaren) {
+        return {
+          ...prev,
+          data: Math.round(tempVar.data * 1.8 + 32),
+          isFaren: true,
+        };
+      }
+      return {
+        ...tempVar,
+        data: Math.round(((tempVar.data - 32) * 5) / 9),
+        isFaren: false,
+      };
+    });
+  };
+
+  useEffect(() => {
+    getData("https://example.com/api/weather");
+  }, []);
+
+  return (
+    <div>
+      <h1>Weather</h1>
+      {weatherData.length === 0 && <p>dats is loading...</p>}
+
+      {weatherData.length > 0 &&
+        weatherData.map((data, index) => (
+          <div key={index}>
+            <p>
+              Temperature: {tempVar.data} {degree}
+            </p>
+            <p>Humidity: {data.humidity}%</p>
+            <p>Wind Speed: {data.windSpeed} km/h</p>
+            <button onClick={toggleValue}>Switch to Fahrenheit</button>
+          </div>
+        ))}
+    </div>
+  );
+};
