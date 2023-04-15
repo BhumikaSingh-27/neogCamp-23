0b03acf0b8b36b3322c29e8d11214e35b944967a
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-04-15 14:13:13 +0530

    created routes

--- useContext-practice/src/App.jsx
+++ useContext-practice/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route, NavLink } from "react-router-dom";
+import { ProductList } from "./pages/ProjectList";
+import { Cart } from "./pages/ProjectList";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <nav>
+        <NavLink to="/"> Home</NavLink> ||
+        <NavLink to="/cart"> Cart</NavLink>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<ProductList />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
     </div>
   );
 }
