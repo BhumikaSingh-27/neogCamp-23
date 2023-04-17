e5432db3f6f24c7e627a0e3aec4b54d88a010264
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-04-17 09:23:02 +0530

    fix

--- FP-Class.js
+++ FP-Class.js
@@ -324,3 +324,35 @@
 
 
 //
+
+ const mostActivePerson = (arr) => {
+    // Your ES6+ code here
+    let sum=0;
+    let max = 0;
+    let maxIndex =0
+    for(let i=0;i<arr.length;i++){
+        const {hours} = arr[i];
+        sum=0
+        for(let j = 0; j<hours.length; j++){
+            sum= sum+hours[j]
+        }
+        if(max<sum){
+            max=sum
+            maxIndex=i
+        }
+    }
+    return arr[maxIndex]
+  };
+  
+  const neogCommunity = [
+    { name: "Raju", role: "student", hours: [1, 2, 3, 1, 2, 3, 0] },
+    { name: "Aakash", role: "mentor", hours: [1, 2, 3, 1, 2, 3, 0] },
+    { name: "Ramesh", role: "student", hours: [1, 2, 3, 1, 2, 3, 3] },
+    { name: "Jiten", role: "TA", hours: [2, 2, 3, 5, 2, 3, 0] },
+    { name: "Harsh", role: "student", hours: [1, 7, 3, 2, 2, 3, 0] },
+    { name: "Akshay", role: "student", hours: [1, 6, 3, 1, 2, 3, 0] },
+    { name: "Rohan", role: "mentor", hours: [1, 2, 3, 12, 2, 3, 0] },
+    { name: "Mohan", role: "student", hours: [1, 8, 3, 0, 2, 3, 0] }
+  ];
+  
+  console.log(mostActivePerson(neogCommunity));
--- react-set-8/src/App.js
+++ react-set-8/src/App.js
@@ -1,21 +1,9 @@
-import "./App.css";
-import { Routes, Route, NavLink } from "react-router-dom";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <NavLink to="/home"> Home</NavLink>||
-        <NavLink to="/cart">MyCart</NavLink>
-        <NavLink to="/wishlist">MyWishlist</NavLink>
-      </nav>
-      <h1>hello</h1>
-      <Routes>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/wishlist" element={<Wishlist />}></Route>
-        <Route path="/home/:itemId" element={<About />}></Route>
-      </Routes>
+      <h1>Hello</h1>
     </div>
   );
 }
