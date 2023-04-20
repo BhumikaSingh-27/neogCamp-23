b382609d01db7ca4cf6fd9c6387b699173f339db
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-04-20 23:41:26 +0530

    fix: updated the logic
    unarchived data is correctly displayed now.

--- react-set-6/src/component/Ques6.jsx
+++ react-set-6/src/component/Ques6.jsx
@@ -2,38 +2,59 @@ import { fakeFetch } from "../api/api6";
 import { useEffect, useState } from "react";
 
 export const Ques6 = () => {
-    const [ taskData, setTaskData ] = useState([])
-    const [ heading, setHeading ] = useState("Unarchived Data")
+  const [taskData, setTaskData] = useState([]);
+  const [heading, setHeading] = useState("Unarchived Data");
 
-    const getData = async(url) =>{
-        try{
-            const response = await fakeFetch(url)
-            if(response.status === 200){
-                setTaskData(response.data.habits)
-            }
-        }catch(e){
-            console.log(e)
-        }
+  const getData = async (url) => {
+    try {
+      const response = await fakeFetch(url);
+      if (response.status === 200) {
+        setTaskData(response.data.habits);
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    useEffect(()=>{
-        getData("https://example.com/api/habits")
-    },[])
+  useEffect(() => {
+    getData("https://example.com/api/habits");
+  }, []);
 
+  const showArchive = () => {
+    setHeading("Archived");
+    console.log("inside funcr", taskData);
+    setTaskData(taskData.filter(({ archived }) => archived));
+  };
 
-    const showArchive = () => {
-        setHeading("Archived")
-        setTaskData(taskData.filter(({archived})=>archived))
-    }
-    return (
-        <>
-        <h2>{heading}</h2>
-        <ul>
-            {
-                taskData.map(({title,desc,daysFollowed,daysSkipped},index)=>(<li key={index}><h2>{title}</h2>{desc}<p>DayFollowed:{daysFollowed}</p><p>Days Skipped: {daysSkipped}</p></li>))
-            }
-        </ul>
-        <button onClick={showArchive}>Show Archive</button>
-        </>
-    )
-}
+  console.log(taskData);
+
+  return (
+    <>
+      <h2>{heading}</h2>
+      <ul>
+        {taskData.map(
+          ({ title, desc, daysFollowed, daysSkipped, archived }, index) => ( !archived &&
+            <li key={index}>
+              <h2>{title}</h2>
+              {desc}
+              <p>DayFollowed:{daysFollowed}</p>
+              <p>Days Skipped: {daysSkipped}</p>
+            </li>
+          )
+        )}
+      </ul>
+      <ul>
+        {heading === "Archived" &&
+          taskData.map(({ title, desc, daysFollowed, daysSkipped }, index) => (
+            <li key={index}>
+              <h2>{title}</h2>
+              {desc}
+              <p>DayFollowed:{daysFollowed}</p>
+              <p>Days Skipped: {daysSkipped}</p>
+            </li>
+          ))}
+      </ul>
+      <button onClick={showArchive}>Show Archive</button>
+    </>
+  );
+};
